2ee7d80a8ae2f8a0e3b6e0d54bb03e36d3096ccb
ARTUR KONDRATEV <92817499+ArturKondratev@users.noreply.github.com>
2022-12-25 12:41:40 +0300

    add task1 (#1)
    * add task1
    
    * fix bag

---
+++ SwiftUI-GB/SwiftUI-GB/SwiftUI_GBApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_GBApp.swift
+//  SwiftUI-GB
+//
+//  Created by Артур Кондратьев on 24.12.2022.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_GBApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView2()
+        }
+    }
+}
